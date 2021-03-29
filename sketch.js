47bf1b26808fe5e7134b431561edc510d7b2e768
aaryanadiger <74605213+aaryanadiger@users.noreply.github.com>
2021-03-29 20:43:56 +0530

    polygon

--- sketch.js
+++ sketch.js
@@ -56,10 +56,10 @@ function setup(){
     // stand2 2nd level 
     block19 = new Box3(894,80,37,30);
 
-    polygon = Bodies.circle(300,200,20);
+    polygon = Bodies.circle(130,200,20,{density:0.05});
     World.add(world,polygon);
 
-    slingshot = new Slingshot(polygon,{x:360, y:200});
+    slingshot = new Slingshot(polygon,{x:130, y:200});
 }
     
 function draw(){
